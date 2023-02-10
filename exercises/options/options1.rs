ddad60508ce2b9c28076bd9c69c248adac509d57
Jun Guo <gjuoun@gmail.com>
2023-02-09 22:26:59 -0400

    Options

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -1,8 +1,6 @@
 // options1.rs
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
 // all, so there'll be no more left :(
@@ -11,14 +9,14 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // The Option output should gracefully handle cases where time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
 
-    /** This is beautiful */
+    // * This is beautiful
     match time_of_day {
         0..=21 => Some(5),
         22..=24 => Some(0),
         _ => None,
     }
 
-    /** Ugly if statement */
+    //? Ugly if statement
     // if time_of_day <= 23 {
     //     Some(5)
     // } else if time_of_day <= 24 {
