06e58c1a30a939c20500eba120dbceb3c929d6c8
Jun Guo <gjuoun@gmail.com>
2023-02-26 23:07:50 -0400

    macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -4,13 +4,25 @@
 
 // I AM NOT DONE
 
+// #[macro_use]
+// mod macros {
+//     macro_rules! my_macro {
+//         () => {
+//             println!("Check out my macro!");
+//         };
+//     }
+// }
+
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
 }
+
+
 
 fn main() {
     my_macro!();
